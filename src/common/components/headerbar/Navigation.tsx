bb68659b6f8b2f2d471a7933b63368e8a1c75410
墨綠BG <s5460703@gmail.com>
2024-04-30 23:07:20 +0800

    🐞 fix：useSearchParams() should be wrapped in a suspense boundary at page

--- src/common/components/headerbar/Navigation.tsx
+++ src/common/components/headerbar/Navigation.tsx
@@ -1,14 +1,18 @@
+import { Suspense } from 'react';
+
 import Logo from './Logo';
 import Menu from './Menu';
 // import Profile from './Profile';
 
 const Navigation: React.FC = () => {
   return (
-    <div className="absolute left-0 top-0 flex h-full w-full items-center">
-      <Logo />
-      <Menu />
-      {/* <Profile /> */}
-    </div>
+    <Suspense>
+      <div className="absolute left-0 top-0 flex h-full w-full items-center">
+        <Logo />
+        <Menu />
+        {/* <Profile /> */}
+      </div>
+    </Suspense>
   );
 };
 
